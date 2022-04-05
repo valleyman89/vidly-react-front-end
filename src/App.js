873319074f713fbafb33cda0269d57e8c6d660b0
valleyman89 <dev@stevenrolph.com>
2022-04-05 14:21:42 +0100

    define start up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Movies from "./components/movies";
+import React from "react";
+import ListGroup from "./components/common/list-group";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <main className="container-fluid">
+        <h1>Vidly</h1>
+        <Movies></Movies>
+      </main>
+    </React.Fragment>
   );
 }
 
