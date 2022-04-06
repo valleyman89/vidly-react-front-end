1081d8119c0be1d625c61dc0f2f3a83eb6a66a07
valleyman89 <dev@stevenrolph.com>
2022-04-06 15:26:14 +0100

    define form input handlers

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,8 +1,53 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
   render() {
-    return <h1>Login Form</h1>;
+    const { account } = this.state;
+    return (
+      <div>
+        <h1>Login Form</h1>
+        <form onSubmit={this.handleSubmit} action="">
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              autoFocus
+              className="form-control"
+              id="username"
+              name="username"
+              onChange={this.handleChange}
+              type="text"
+              value={account.username}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              className="form-control"
+              id="password"
+              name="password"
+              onChange={this.handleChange}
+              type="text"
+              value={account.password}
+            />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
   }
 }
 
