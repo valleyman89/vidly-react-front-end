f647ec8e7cfc3fc84a7623faca4da506779d2d73
valleyman89 <dev@stevenrolph.com>
2022-04-08 16:38:00 +0300

    build registerForm

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -3,9 +3,26 @@ import Form from "./common/form";
 import Joi from "joi-browser";
 
 class Register extends Form {
-  state = {};
+  state = { data: { username: "", password: "", name: "" }, errors: {} };
+
+  schema = {
+    username: Joi.string().email().required().label("username"),
+    password: Joi.string().required().min(5).label("password"),
+    name: Joi.string().required().label("name"),
+  };
+
   render() {
-    return <h1>Registration</h1>;
+    return (
+      <div>
+        <h1>Registration</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username", "email")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
   }
 }
 
