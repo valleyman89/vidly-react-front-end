06bfee3694d5abb2ef119068500a6877a0f777c0
valleyman89 <dev@stevenrolph.com>
2022-04-10 11:50:59 +0300

    remove placeholder functionality

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from "react";
 
-const Input = ({ name, label, error, placeholder, ...rest }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        {...rest}
-        className="form-control"
-        id={name}
-        name={name}
-        placeholder={placeholder}
-      />
+      <input {...rest} className="form-control" id={name} name={name} />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
