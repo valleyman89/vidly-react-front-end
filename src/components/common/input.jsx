e0b04187edd24e66657c875999ae39691a8e592f
valleyman89 <dev@stevenrolph.com>
2022-04-10 11:12:52 +0300

    add placeholder prop

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
-const Input = ({ name, label, error, ...rest }) => {
+const Input = ({ name, label, error, placeholder, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input {...rest} className="form-control" id={name} name={name} />
+      <input
+        {...rest}
+        className="form-control"
+        id={name}
+        name={name}
+        placeholder={placeholder}
+      />
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
