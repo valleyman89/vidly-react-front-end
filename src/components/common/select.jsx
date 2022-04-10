c668b7cf9e96f9ea494687a45f0b3e5c205683de
valleyman89 <dev@stevenrolph.com>
2022-04-10 10:45:45 +0300

    define movie form and select input

--- src/components/common/select.jsx
+++ src/components/common/select.jsx
@@ -4,12 +4,11 @@ const Select = ({ name, label, options, error, ...rest }) => {
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
       <select name={name} id={name} {...rest} className="form-control">
-        <option value=""></option>
-        {options.map((option) => {
+        {options.map((option) => (
           <option key={option._id} value={option._id}>
             {option.name}
-          </option>;
-        })}
+          </option>
+        ))}
       </select>
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
