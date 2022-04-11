a7a1b4bfa32a18731e8c76dd4f06140fe1a8cf97
valleyman89 <dev@stevenrolph.com>
2022-04-11 16:20:55 +0300

    include interceptor and import toastify

--- src/services/http.js
+++ src/services/http.js
@@ -1,16 +1,17 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
-// axios.interceptors.response.use(null, (error) => {
-//   const expectedError =
-//     error.response &&
-//     error.response.status >= 400 &&
-//     error.response.status < 500;
+axios.interceptors.response.use(null, (error) => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
 
-//   if (!expectedError) {
-//     toast.error("An unexpected error occured.");
-//   }
-//   return Promise.reject(error);
-// });
+  if (!expectedError) {
+    toast.error("An unexpected error occured.");
+  }
+  return Promise.reject(error);
+});
 
 export default {
   get: axios.get,
