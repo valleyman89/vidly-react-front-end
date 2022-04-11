bc5a6a7c1aef150c517cf26f093703ea9622d249
valleyman89 <dev@stevenrolph.com>
2022-04-11 16:10:22 +0300

    define getMovie and saveMovie functions

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,16 @@
 import http from "./http";
 const apiEndpoint = "http://localhost:5010/api/movies/";
+
 export function getMovies() {
   return http.get(apiEndpoint);
+}
+
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + "/" + movieId);
+}
+
+export function saveMovie(movieId) {
+  return http.put(apiEndpoint + "/" + movieId);
 }
 
 export function deleteMovie(movieId) {
