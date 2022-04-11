0034d36fd151cb76e2f31064d8de7a298901e8fa
valleyman89 <dev@stevenrolph.com>
2022-04-11 16:49:15 +0300

    fix redirect if movieId does not exist

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -35,10 +35,13 @@ class MovieForm extends Form {
     const movieId = this.props.match.params.id;
     if (movieId === "new") return;
 
-    const { data: movie } = await getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
-
-    this.setState({ data: this.mapToViewModel(movie) });
+    try {
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace("/not-found");
+    }
   }
 
   mapToViewModel(movie) {
