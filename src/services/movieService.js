f4e719b3ae73914196c03263700843faa58fdb38
valleyman89 <dev@stevenrolph.com>
2022-04-11 16:48:55 +0300

    refactor endpoint calls

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,20 +1,20 @@
 import http from "./http";
 import config from "../config.json";
 
-const apiEndpoint = config.apiUrl + "/movies";
+const apiEndpoint = config.apiUrl + "/movies/";
 
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + "/" + movieId);
+  return http.get(apiEndpoint + movieId);
 }
 
-export function saveMovie(movieId) {
-  return http.put(apiEndpoint + "/" + movieId);
+export function saveMovie(movie) {
+  // return http.put(apiEndpoint + movieId);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(apiEndpoint + movieId);
 }
