96a5a3ed764d8cce2517546cdf1687872f703a73
valleyman89 <dev@stevenrolph.com>
2022-04-11 19:38:15 +0300

    refactor endpoint calls

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -8,24 +8,29 @@ const options = {
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjRkYzk0YjI0Mzc3ZGFjNzkxZjFmNWUiLCJpYXQiOjE2NDkyNjU2MDB9.kzxxY0xLPQamPcafhKFdwxgxgGeBWHyLG4vpGMc7XBg",
   },
 };
+
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(apiEndpoint + movie._id, body, options);
+    return http.put(movieUrl(movie._id), body, options);
   }
 
   return http.post(apiEndpoint, movie, options);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + movieId, options);
+  return http.delete(movieUrl(movieId), options);
 }
