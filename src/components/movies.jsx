d557d0be6448c630ceac150cfb4ad04085f857a3
valleyman89 <dev@stevenrolph.com>
2022-04-12 14:52:47 +0300

    hide new movie button if logged out

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -103,6 +103,7 @@ class Movies extends Component {
   render() {
     const { length: count, searchQuery } = this.state.movies;
     const { pageSize, currentPage, sortColumn } = this.state;
+    const { user } = this.props;
 
     if (count === 0) return <p>There are no movies in the database.</p>;
 
@@ -119,13 +120,15 @@ class Movies extends Component {
         </div>
         <div className="col">
           <div>
-            <Link
-              to="/movies/new"
-              className="btn btn-primary"
-              style={{ marginBottom: 20 }}
-            >
-              New Movie
-            </Link>
+            {user && (
+              <Link
+                to="/movies/new"
+                className="btn btn-primary"
+                style={{ marginBottom: 20 }}
+              >
+                New Movie
+              </Link>
+            )}
           </div>
           <span>Showing {totalCount} movies in the database.</span>
           <SearchBox value={searchQuery} onChange={this.handleSearch} />
