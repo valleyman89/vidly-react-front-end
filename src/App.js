8366b03c8c3df3f4b778335892e840ad39853861
valleyman89 <dev@stevenrolph.com>
2022-04-12 12:41:36 +0300

    grab auth token, sort imports

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
 import { Route, Switch, Redirect } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Customers from "./components/customers";
+import jwtDecode from "jwt-decode";
 import LoginForm from "./components/loginForm";
 import MovieForm from "./components/movieForm";
 import Movies from "./components/movies";
 import Navbar from "./components/Navbar";
 import NotFound from "./components/notFound";
-import React from "react";
-import Rentals from "./components/rentals";
+import React, { Component } from "react";
 import RegisterForm from "./components/registerForm";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import Rentals from "./components/rentals";
+import { toHaveStyle } from "@testing-library/jest-dom/dist/matchers";
 
-function App() {
-  return (
-    <React.Fragment>
-      <ToastContainer />
-      {/* call navbar component */}
-      <Navbar />
-      <main className="container-fluid">
-        {/* switch */}
-        <Switch>
-          <Route path="/movies/:id" component={MovieForm}></Route>
-          <Route path="/login" component={LoginForm}></Route>
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/movies/new" component={MovieForm} />
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (error) {}
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        {/* call navbar component */}
+        <Navbar user={this.state.user} />
+        <main className="container-fluid">
+          {/* switch */}
+          <Switch>
+            <Route path="/movies/:id" component={MovieForm}></Route>
+            <Route path="/login" component={LoginForm}></Route>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/movies/new" component={MovieForm} />
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
