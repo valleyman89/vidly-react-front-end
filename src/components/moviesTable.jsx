22d151d67f5090c78ecbb8f97fee7506d516b467
valleyman89 <dev@stevenrolph.com>
2022-04-12 16:03:00 +0300

    hide delete button if user not admin or logged out

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Like from "./common/like";
 import Table from "./common/table";
 import Link from "react-router-dom/Link";
-
+import auth from "../services/authService";
 class MoviesTable extends Component {
   columns = [
     {
@@ -26,20 +26,29 @@ class MoviesTable extends Component {
         />
       ),
     },
-    {
-      key: "delete",
-      content: (movie) => (
-        <button
-          onClick={() => {
-            this.props.onDelete(movie);
-          }}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      ),
-    },
   ];
+
+  deleteColumn = {
+    key: "delete",
+    content: (movie) => (
+      <button
+        onClick={() => {
+          this.props.onDelete(movie);
+        }}
+        className="btn btn-danger btn-sm"
+      >
+        Delete
+      </button>
+    ),
+  };
+
+  constructor() {
+    super();
+    const user = auth.getCurrentUser();
+    if (user && user.isAdmin) {
+      this.columns.push(this.deleteColumn);
+    }
+  }
   render() {
     const { movies, sortColumn, onSort } = this.props;
     return (
